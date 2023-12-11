03d91104d8dcbcd2aef4e908774fa14c354c2c51
Dario200225 <dgirlanda2002@gmail.com>
2023-12-11 12:07:16 +0100

    Implemented GetInsult Service

--- Insults/src/app/app.component.ts
+++ Insults/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { GetInsultService } from './get-insult.service';
 
 @Component({
   selector: 'app-root',
--- Insults/src/app/get-insult.service.ts
+++ Insults/src/app/get-insult.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,14 @@ import { Injectable } from '@angular/core';
 export class GetInsultService {
 
   constructor() { }
+
+  async Insult(language : String, type : String) : Promise<String> {
+
+    language = "en";
+    type = "default";
+
+    let ins = await axios.get('https://evilinsult.com/generate_insult.php?lang=' + language + '&type=' + type);
+
+    return ins.data['insult'];
+  }
 }
