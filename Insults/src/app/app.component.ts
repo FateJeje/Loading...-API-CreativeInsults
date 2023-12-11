afcbc367f56d4ec5ca896dd7abc2812fb3d4a206
TheW0k <matt.giuriato@gmail.com>
2023-12-11 12:50:16 +0100

    InsultComponent

--- Insults/src/app/app.component.ts
+++ Insults/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< Updated upstream
 import { GetInsultService } from './get-insult.service';
+=======
+import { InsultGeneratorComponent } from './insult-generator/insult-generator.component';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InsultGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Insults/src/app/insult-generator/insult-generator.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'insult-generator',
+  standalone: true,
+  imports: [],
+  templateUrl: './insult-generator.component.html',
+  styleUrl: './insult-generator.component.css'
+})
+
+export class InsultGeneratorComponent {
+
+}
