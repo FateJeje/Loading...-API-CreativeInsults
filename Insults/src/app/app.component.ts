119555e28810db9b5ec701141760d533fa90afa3
Dario200225 <dgirlanda2002@gmail.com>
2023-12-18 10:08:48 +0100

    commit changes

--- Insults/src/app/app.component.ts
+++ Insults/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< Updated upstream
-import { GetInsultService } from './get-insult.service';
-=======
-import { InsultGeneratorComponent } from './insult-generator/insult-generator.component';
->>>>>>> Stashed changes
+import { InsultsGeneratorComponent } from './insults-generator/insults-generator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, InsultGeneratorComponent],
+  imports: [CommonModule, RouterOutlet, InsultsGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
